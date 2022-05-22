acbdf3b3d1ecacfb71e865cf4ee2e4fc400bde7f
wrenfannnin <luwr5102@gmail.com>
2022-05-22 12:22:11 +0100

    Python example written

--- serialRead.py
+++ serialRead.py
@@ -1,1 +1,10 @@
-import serial
+import serial as ser
+import time
+
+ser = serial.Serial('/dev/ttyACM0',115200, timeout=10) # IMPORTANT, CHANGE ACM0 to USB0 OR OTHER AS APPROPIRATE
+ser.open()
+data = ser.read()
+while True:
+    time.sleep(0.5)
+    print(data)
+    time.sleep(0.5)
