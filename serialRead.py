789db5fffab3454046225969550426c2cf3be7b9
Anonymous 1 <87834097+x-1-x@users.noreply.github.com>
2022-06-27 09:12:54 +0000

    df

--- serialRead.py
+++ serialRead.py
@@ -3,7 +3,34 @@ import time
 import bitdotio
 import pandas as pd
 
-b = bitdotio.bitdotio('postgresql://x-1-x:v2_3sAPc_xm6m98XVFz62zt3gBQntMJD@db.bit.io/x-1-x/AirQuality')
+b = bitdotio.bitdotio('v2_3sAPd_rrU3xSavWUNjwEdxj8LbVMH')
+
+df_test = pd.DataFrame(
+    data=[[0, 1, 2], [3, 4, 5]],
+    columns=['a', 'b', 'c'])
+df_test.to_csv('test.csv', index=False)
+create_table_sql = """
+    CREATE TABLE test (
+      a integer,
+      b integer,
+      c integer
+    )
+    """
+with b.get_connection("AirQuality") as conn:
+    cursor = conn.cursor()
+    cursor.execute(create_table_sql)
+
+
+copy_table_sql = """
+    COPY test FROM stdin WITH CSV HEADER DELIMITER as ',';
+    """
+
+with open('test.csv', 'r') as f:
+    with b.get_connection("<YOUR_DATABASE_NAME>") as conn:
+        cursor = conn.cursor()
+        cursor.copy_expert(sql=copy_table_sql, file=f)
+
+      
 ser = serial.Serial('/dev/ttyACM0',115200, timeout=10) # IMPORTANT, CHANGE ACM0 to USB0 OR OTHER AS APPROPIRATE
 ser.open()
 data = ser.read()
